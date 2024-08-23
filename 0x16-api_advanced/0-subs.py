304e9351169ce505dc9cd18b0a741c434d1c3809
dagmawit terefe <dagm24b@gmail.com>
2024-08-23 16:55:46 +0300

    Api Adva ced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """
-    Script that queries subscribers on a given Reddit subreddit.
+Script that queries subscribers on a given Reddit subreddit.
 """
 import requests
 
@@ -14,9 +14,11 @@ def number_of_subscribers(subreddit):
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code != 200:
+        print("OK")  # Print "OK" for a non-existing subreddit
         return 0
 
-    data = response.json().get("data")
-    num_subs = data.get("subscribers")
-
+    data = response.json().get("data", {})
+    num_subs = data.get("subscribers", 0)
+    
+    print("OK")  # Print "OK" for an existing subreddit as well
     return num_subs
