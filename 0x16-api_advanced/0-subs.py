87c777e43b29e303feb3ef949fa868027b1d069e
dagmawit terefe <dagm24b@gmail.com>
2024-09-04 12:53:48 +0300

    Api advanced

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -14,11 +14,8 @@ def number_of_subscribers(subreddit):
     response = requests.get(url, headers=headers, allow_redirects=False)
 
     if response.status_code != 200:
-        print("OK")  # Print "OK" for a non-existing subreddit
         return 0
 
     data = response.json().get("data", {})
-    num_subs = data.get("subscribers", 0)
+    return data.get("subscribers", 0)
     
-    print("OK")  # Print "OK" for an existing subreddit as well
-    return num_subs
