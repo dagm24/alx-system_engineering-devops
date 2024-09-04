f15adb683116eef0c8df414fc293d01a0f8bd243
dagmawit terefe <dagm24b@gmail.com>
2024-09-04 13:28:55 +0300

    Api advanced

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,24 +1,22 @@
 #!/usr/bin/python3
-'''
-    this module contains the function top_ten
-'''
+"""Module to query Reddit API and print titles of top 10 hot posts"""
 import requests
-from sys import argv
 
 
 def top_ten(subreddit):
-    '''
-        returns the top ten posts for a given subreddit
-    '''
-    user = {'User-Agent': 'Lizzie'}
-    url = requests.get('https://www.reddit.com/r/{}/hot/.json?limit=10'
-                       .format(subreddit), headers=user).json()
-    try:
-        for post in url.get('data').get('children'):
-            print(post.get('data').get('title'))
-    except Exception:
+    """
+    Queries the Reddit API and prints the titles of the first 10 hot posts
+    for a given subreddit.
+    """
+    url = f"https://www.reddit.com/r/{subreddit}/hot.json?limit=10"
+    headers = {'User-Agent': 'Custom User Agent 1.0'}
+
+    response = requests.get(url, headers=headers, allow_redirects=False)
+
+    if response.status_code == 200:
+        data = response.json().get('data', {})
+        posts = data.get('children', [])
+        for post in posts:
+            print(post['data']['title'])
+    else:
         print(None)
-
-
-if __name__ == "__main__":
-    top_ten(argv[1])
