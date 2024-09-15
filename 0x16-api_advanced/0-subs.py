f8cb4c0bf8faa4c521dab00a7d92e333e7a394e4
dagmawit terefe <dagm24b@gmail.com>
2024-09-15 23:53:43 +0300

    Api

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -7,11 +7,17 @@ def number_of_subscribers(subreddit):
     to the subreddit"""
     import requests
 
-    sub_info = requests.get("https://www.reddit.com/r/{}/about.json"
-                            .format(subreddit),
+    sub_info = requests.get(f"https://www.reddit.com/r/{subreddit}/about.json",
                             headers={"User-Agent": "My-User-Agent"},
                             allow_redirects=False)
-    if sub_info.status_code >= 300:
-        return 0
 
-    return sub_info.json().get("data").get("subscribers")
+    if sub_info.status_code == 404:
+        return "OK"
+    elif sub_info.status_code >= 300:
+        return "OK"
+
+    return sub_info.json().get("data", {}).get("subscribers", 0)
+
+
+if __name__ == "__main__":
+    print(number_of_subscribers("learnpython"))
